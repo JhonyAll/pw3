baa4325ec9c0b0ed4166de5787283ba5317f789e
JhonyAL <kakashi3491@gmail.com>
2024-04-09 10:34:24 -0300

    fsad

--- funcao.js
+++ funcao.js
@@ -1,16 +1,26 @@
 async function post_Dados(url = "", data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: "POST", 
-      mode: "no-cors", 
-      cache: "no-cache", 
-      credentials: "omit", 
-      headers: {
-        "Content-Type": "application/json"
-      },
-      redirect: "follow", 
-      referrerPolicy: "no-referrer", 
-      body: JSON.stringify(data), 
-    });
-    return response.json(); 
+
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json', 'User-Agent': 'insomnia/2023.5.8'},
+    body: JSON.stringify(data)
+  };
+  
+  return await fetch('http://localhost:3000/produto', options)
+    .then(response => response.json())
+  // Default options are marked with *
+  // return await fetch(url, {
+  //   method: "POST",
+  //   mode: "no-cors",
+  //   cache: "no-cache",
+  //   credentials: "omit",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Access-Control-Allow-Origin": "*",
+  //     "Access-Control-Allow-Credentials": true,
+  //   },
+  //   redirect: "follow",
+  //   referrerPolicy: "no-referrer",
+  //   body: JSON.stringify(data),
+  // }).then((response) => response.json());
 }
